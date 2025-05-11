5abd99ec5f2d934c9aa265749d729b7dbb295089
Yaroslav <yaroslav.shebovovkk@gmail.com>
2025-05-11 21:28:56 +0200

    set up express/prisma app, created necessary folders

---
+++ app.js
@@ -0,0 +1,11 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.listen(PORT, () => {
+    console.log(`App is running on port ${PORT}`)
+})
